043037aecfb31de408a555db45db4e19f74e0e47
vincentvankoh <yunskoh@gmail.com>
2020-12-09 12:00:43 -0500

    cross-browser comptaibility added

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,4 @@ gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
 gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
 gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+
